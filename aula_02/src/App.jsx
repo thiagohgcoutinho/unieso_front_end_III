21c69c9e6d755eec7188f2fa5548358f4e75ed7c
Thiago Coutinho <2022211510098@iesp.edu.br>
2024-02-28 21:40:33 -0300

    Aula_02

---
+++ aula_02/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Exercicio1Adicao from './components/Exercicio1Adicao'
+import Exercicio1Subtracao from './components/Exercicio1Subtracao'
+import Exercicio1Multiplicacao from './components/Exercicio1Multiplicacao'
+import Exercicio1Divisao from './components/Exercicio1Divisao'
+
+const App = () => {
+  return (
+    <div>
+      <Exercicio1Adicao num1={2} num2={3}/>
+      <Exercicio1Subtracao num1={5} num2={2}/>
+      <Exercicio1Multiplicacao num1={3} num2={6}/>
+      <Exercicio1Divisao num1={15} num2={3}/>
+    </div>
+  )
+}
+
+export default App
