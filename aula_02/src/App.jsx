9140baaed4006dce987063175432b560c9846ab5
Thiago Coutinho <2022211510098@iesp.edu.br>
2024-03-01 20:25:27 -0300

    Aula 02

--- aula_02/src/App.jsx
+++ aula_02/src/App.jsx
@@ -3,6 +3,7 @@ import Exercicio1Adicao from './components/Exercicio1Adicao'
 import Exercicio1Subtracao from './components/Exercicio1Subtracao'
 import Exercicio1Multiplicacao from './components/Exercicio1Multiplicacao'
 import Exercicio1Divisao from './components/Exercicio1Divisao'
+import PrecisoEstudar from './components/PrecisoEstudar'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Exercicio1Subtracao num1={5} num2={2}/>
       <Exercicio1Multiplicacao num1={3} num2={6}/>
       <Exercicio1Divisao num1={15} num2={3}/>
+      <PrecisoEstudar nomeDaTecnologia={"Front_end_III"}/>
     </div>
   )
 }
---
+++ aula_02/src/components/PrecisoEstudar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PrecisoEstudar = (props) => {
+  return (
+    <div>
+        <h1>
+            Preciso estudar {props.nomeDaTecnologia}!
+        </h1>
+    </div>
+  )
+}
+
+export default PrecisoEstudar
